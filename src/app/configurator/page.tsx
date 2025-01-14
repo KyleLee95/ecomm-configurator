fdf1e10888c54ee6ccb28146042371f09aa3172f
kyle <kyle@kylelee.dev>
2025-01-14 13:59:15 -0500

    add basic three scene

---
+++ src/app/configurator/page.tsx
@@ -0,0 +1,61 @@
+"use client"
+import { useRef, useState } from "react"
+import { useFrame, Canvas } from "@react-three/fiber"
+import Header from "@/components/header"
+import { ResizablePanel, ResizablePanelGroup, ResizableHandle } from "@/components/ui/resizable"
+function Box(props) {
+	// This reference gives us direct access to the THREE.Mesh object
+	const ref = useRef()
+	// Hold state for hovered and clicked events
+	const [hovered, hover] = useState(false)
+	const [clicked, click] = useState(false)
+	// Subscribe this component to the render-loop, rotate the mesh every frame
+	useFrame((state, delta) => (ref.current.rotation.x += delta))
+	// Return the view, these are regular Threejs elements expressed in JSX
+	return (
+		<mesh
+			{...props}
+			ref={ref}
+			scale={clicked ? 1.5 : 1}
+			onClick={(event: Event) => click(!clicked)}
+			onPointerOver={(event: Event) => hover(true)}
+			onPointerOut={(event: Event) => hover(false)}
+		>
+			<boxGeometry args={[1, 1, 1]} />
+			<meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+		</mesh>
+	)
+}
+
+export default function Page() {
+	return (
+		<>
+			<Header />
+			<main className="h-full w-full">
+				<ResizablePanelGroup direction="horizontal">
+					<ResizablePanel>
+						<Canvas>
+							<ambientLight intensity={Math.PI / 2} />
+							<spotLight
+								position={[10, 10, 10]}
+								angle={0.15}
+								penumbra={1}
+								decay={0}
+								intensity={Math.PI}
+							/>
+							<pointLight
+								position={[-10, -10, -10]}
+								decay={0}
+								intensity={Math.PI}
+							/>
+							<Box position={[-1.2, 0, 0]} />
+							<Box position={[1.2, 0, 0]} />
+						</Canvas>
+					</ResizablePanel>
+					<ResizableHandle withHandle />
+					<ResizablePanel>Two</ResizablePanel>
+				</ResizablePanelGroup>
+			</main>
+		</>
+	)
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,51 @@
+import { ModeToggle } from "./mode-toggle"
+
+const items = [
+	{
+		display: "Home",
+		url: "/",
+	},
+	{
+		display: "Chairs",
+		url: "/shop/chairs",
+	},
+	{
+		display: "Tables",
+		url: "/shop/Tables",
+	},
+]
+
+const userLinks = [
+	{
+		display: "Cart",
+		url: "/cart",
+		icon: "",
+	},
+	{
+		display: "Account",
+		url: "/account",
+		icon: "",
+	},
+]
+const Header = () => {
+	return (
+		<nav className="flex justify-around my-5">
+			{items.map((item) => (
+				<a key={item.url} href={item.url}>
+					{item.display}
+				</a>
+			))}
+			<div>
+				{userLinks.map((userLink) => (
+					<a key={userLink.url} href={userLink.url}>
+						{" "}
+						{userLink.display}
+					</a>
+				))}
+			</div>
+			<ModeToggle />
+		</nav>
+	)
+}
+
+export default Header
